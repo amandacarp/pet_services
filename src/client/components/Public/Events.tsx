cf9563f6a0a6015ddae3f70359758534b0911b20
Amanda Carpentieri <carpentieri.a@gmail.com>
2021-04-13 10:24:17 -0700

    style edits

--- src/client/components/Public/Events.tsx
+++ src/client/components/Public/Events.tsx
@@ -22,37 +22,41 @@ const Events = (props: EventsProps) => {
 
             <div className="flex flex-wrap justify-center px-2 mx-auto mt-10 overflow-hidden">
                 <div className="px-5 py-5 m-2 text-center text-gray-800 bg-gray-300 border border-indigo-300 rounded shadow">
-                    {events === null ? <h1>No Appointments</h1> : 
-                    <table className="w-1/2 text-xs table-auto md:text-lg lg:w-full">
-                        <thead>
-                            <tr>
-                                <th className="text-center md:text-xl">Owner</th>
-                                <th className="text-center md:text-xl">Pet</th>
-                                <th className="text-center md:text-xl">Title</th>
-                                <th className="text-center md:text-xl">Service</th>
-                                <th className="text-center md:text-xl">Date(s)</th>
-                                <th className="text-center md:text-xl">Time</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {events?.map((event: Event) => {
-                                return (
-                                    <tr key={event?.id}>
-                                        <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.owner_name}</td>
-                                        <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.pet_name}</td>
-                                        <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.title}</td>
-                                        <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.name}</td>
-                                        <td className="text-center border border-indigo-600 md:px-2 md:py-2">{moment(event?.start_date).add(10, 'days').calendar()} {event?.end_date === null ? '' : 'to ' + moment(event?.end_date).add(10, 'days').calendar()}</td>
-                                        <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.time}</td>
+                    {events === null ? <h1>No Appointments</h1> :
+                        <table className="w-1/2 text-xs table-auto md:text-lg lg:w-full">
+                            <thead>
+                                <tr>
+                                    <th className="text-center md:text-xl">Owner</th>
+                                    <th className="text-center md:text-xl">Pet</th>
+                                    <th className="text-center md:text-xl">Title</th>
+                                    <th className="text-center md:text-xl">Service</th>
+                                    <th className="text-center md:text-xl">Date(s)</th>
+                                    <th className="text-center md:text-xl">Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {events?.map((event: Event) => {
+                                    return (
+                                        <tr key={event?.id}>
+                                            <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.owner_name}</td>
+                                            <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.pet_name}</td>
+                                            <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.title}</td>
+                                            <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.name}</td>
+                                            <td className="text-center border border-indigo-600 md:px-2 md:py-2">{moment(event?.start_date).add(10, 'days').calendar()} {event?.end_date === null ? '' : 'to ' + moment(event?.end_date).add(10, 'days').calendar()}</td>
+                                            <td className="text-center border border-indigo-600 md:px-2 md:py-2">{event?.time}</td>
 
-                                    </tr>
-                                )
+                                        </tr>
+                                    )
 
-                            })}
-                        </tbody>
-                    </table>
+                                })}
+                            </tbody>
+                        </table>
                     }
-                    <div className="mt-5 text-center md:px-2 md:py-2"><button className="inline-block px-4 py-2 mt-4 text-sm leading-none text-indigo-400 border border-indigo-400 rounded hover:border-transparent hover:text-indigo-500 hover:bg-white md:mt-0"><Link to={`/add/event`}>Schedule</Link></button></div>
+                    <div className="mt-5 text-center md:px-2 md:py-2">
+                        <button className="inline-block px-4 py-2 mt-4 text-sm leading-none text-indigo-400 border border-indigo-400 rounded hover:border-transparent hover:text-indigo-500 hover:bg-white md:mt-0">
+                            <Link to={`/add/event`}>Schedule</Link>
+                        </button>
+                    </div>
 
                 </div>
             </div>
